d888a98604133de8a1cb539d67b45f3b3f2cb8fe
Deepak Manoj <43246569+deepakdmk@users.noreply.github.com>
2025-05-06 18:48:11 +0800

    Add files via upload

---
+++ PaymentSystem/src/ConsoleTemplates.java
@@ -0,0 +1,23 @@
+public class ConsoleTemplates {
+
+    private ConsoleTemplates() {}
+
+    public static final String MAIN_MENU= "Welcome to AwesomeGIC Bank! What would you like to do?";
+    public static final String MAIN_MENU_OPTION_TRANSACTION= "[T] Input transactions";
+    public static final String MAIN_MENU_OPTION_INTEREST= "[I] Define interest rules";
+    public static final String MAIN_MENU_OPTION_STATEMENT= "[P] Print statement";
+    public static final String MAIN_MENU_OPTION_QUIT= "[Q] Quit";
+    public static final String MAIN_MENU_SYMBOL_TRANSACTION= "t";
+    public static final String MAIN_MENU_SYMBOL_INTEREST= "i";
+    public static final String MAIN_MENU_SYMBOL_STATEMENT= "p";
+    public static final String MAIN_MENU_SYMBOL_QUIT= "q";
+    public static final String MAIN_MENU_EXIT="Thank you for banking with AwesomeGIC Bank.\nHave a nice day!";
+
+
+    public static final String TRANSACTION_HEADER="Please enter transaction details in <Date> <Account> <Type> <Amount> format (or enter blank to go back to main menu):";
+
+
+    public static final String ERROR_INPUT="Invalid input";
+
+
+}
